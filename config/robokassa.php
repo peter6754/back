50719aca256334111fe22cc84ef0e34f752ba9f2
peter6754 <p1312402@gmail.com>
2024-10-31 06:28:19 +0000

    add new routes

--- config/robokassa.php
+++ config/robokassa.php
@@ -2,6 +2,6 @@
 
 return [
     'merchant_login' => env('ROBOKASSA_MERCHANT_LOGIN'),
-    'password1'     => env('ROBOKASSA_PASSWORD1'),
-    'password2'     => env('ROBOKASSA_PASSWORD2'),
+    'password1' => env('ROBOKASSA_PASSWORD1'),
+    'password2' => env('ROBOKASSA_PASSWORD2'),
 ];
