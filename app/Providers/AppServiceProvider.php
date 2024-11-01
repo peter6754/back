d6866f2542235caf939fcc81fe07373d5b2cdec3
peter6754 <p1312402@gmail.com>
2024-11-01 19:01:37 +0000

    Update routes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (empty(env('APP_URL'))) {
+            $schema = request()->isSecure() ? 'https://' : 'http://';
+            \URL::forceRootUrl($schema . request()->getHttpHost());
+        }
     }
 }
