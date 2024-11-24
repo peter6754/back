ef6fec0ab73712c53725e0e2299323f1db917690
peter6754 <p1312402@gmail.com>
2024-11-24 11:55:44 +0000

    Update payments type

--- database/migrations/2025_05_18_172838_create_transaction_processes_table.php
+++ database/migrations/2025_05_18_172838_create_transaction_processes_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('provider', 255)->nullable()->collation('utf8mb4_unicode_ci');
             $table->string('user_id', 191)->nullable()->collation('utf8mb4_unicode_ci')->index('transactions_user_id_fkey');
             $table->double('price')->nullable();
+            $table->string('type', 255)->nullable()->collation('utf8mb4_unicode_ci');
             $table->string('status', 255)->default(PaymentsService::ORDER_STATUS_PENDING)->collation('utf8mb4_unicode_ci');
             $table->string('email', 255)->nullable()->collation('utf8mb4_unicode_ci');
             $table->timestamp('purchased_at', 3)->useCurrent();
