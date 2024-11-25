83e3f04d7808a6832126b6c507e843055ac3115e
peter6754 <p1312402@gmail.com>
2024-11-25 19:54:04 +0000

    Remove trash logs

--- app/Services/Payments/Providers/RobokassaProvider.php
+++ app/Services/Payments/Providers/RobokassaProvider.php
@@ -288,14 +288,10 @@ class RobokassaProvider implements PaymentProviderInterface
                     // Default variable
                     $updateParams = [];
 
-                    Log::channel($this->getProviderName())->info(__METHOD__ . ':' . __LINE__, [$transaction]);
-
                     // Calculate current options
                     if (!empty($transaction['package_type']) && !empty($transaction['package_count'])) {
                         $updateParams[$transaction['package_type']] = $transaction['package_count'];
                         $updateParams['user_id'] = $transaction['user_id'];
-
-                        Log::channel($this->getProviderName())->info(__METHOD__ . ':' . __LINE__, [$updateParams]);
 
                         // Update params
                         $this->payments->sendServicePackage($updateParams);
