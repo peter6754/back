093bdf9c76c50839cb06fc3c16a8680545440f08
peter6754 <p1312402@gmail.com>
2024-11-28 13:02:22 +0000

    Hot fix test

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->validateCsrfTokens(except: [
             'payment/*',
             'auth/*',
+            '*',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
