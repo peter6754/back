b34caaa6b4ef547842c02510a69c4cab937ee5ef
peter6754 <p1312402@gmail.com>
2024-12-06 10:24:48 +0000

    Test recomendations with AI

---
+++ app/Http/Controllers/Recommendations/RecommendationsController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Recommendations;
+
+use App\Http\Traits\ApiResponseTrait;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Services\RecommendationService;
+
+class RecommendationsController extends Controller
+{
+    use ApiResponseTrait;
+
+
+    public function getTopProfiles(Request $request)
+    {
+        // Checking auth user
+        $customer = $this->checkingAuth();
+
+
+        if (!empty($request['join'])) {
+            $data = RecommendationService::getPotentialMatchesOptimized($customer);
+        } else {
+            $data = RecommendationService::getPotentialMatches($customer);
+        }
+        var_dump($data);
+    }
+}
