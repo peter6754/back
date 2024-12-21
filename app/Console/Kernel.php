fcac39ed3e307539258b5124766b9e7670abf9f0
peter6754 <p1312402@gmail.com>
2024-12-21 17:43:21 +0000

    Update schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -10,23 +10,22 @@ class Kernel extends ConsoleKernel
 
     /**
      * Список Artisan команд, предоставляемых вашим приложением.
-     *
      * @var array
      */
     protected $commands = [
         \App\Console\Commands\ProcessMailQueue::class
     ];
+
     protected function schedule(Schedule $schedule)
     {
-
-// Обработка очереди писем каждые 5 минут
+        // Обработка очереди писем каждые 5 минут
         $schedule->command('mail:process-queue')
             ->everyFiveMinutes()
             ->withoutOverlapping();
 
-// Очистка старых писем каждый день в 2:00
-//        $schedule->command('mail:clean-old --days=30')
-//            ->dailyAt('02:00');
+        // Очистка старых писем каждый день в 2:00
+        //        $schedule->command('mail:clean-old --days=30')
+        //            ->dailyAt('02:00');
     }
 
 }
