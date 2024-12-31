3b2c3a6f0a8416e4aa5bc43adb8901c26ddd2246
peter6754 <p1312402@gmail.com>
2024-12-31 05:34:34 +0000

    Upload subscription price method

--- app/Http/Controllers/Application/PricesController.php
+++ app/Http/Controllers/Application/PricesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Application;
 
 use Exception;
 use Illuminate\Http\Request;
+use App\Models\Subscriptions;
 use App\Models\ServicePackages;
 use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
@@ -12,6 +13,108 @@ use App\Http\Traits\ApiResponseTrait;
 class PricesController extends Controller
 {
     use ApiResponseTrait;
+
+    /**
+     * @param Request $request
+     * @param $id
+     * @OA\Get(
+     *     path="/application/prices/subscriptions/{id}",
+     *     tags={"App Settings"},
+     *     summary="Price for subscriptions",
+     *     security={{"bearerAuth":{}}},
+     *
+     *     @OA\Parameter(
+     *           name="id",
+     *           in="path",
+     *           description="Type (на backend-е конвертируем в numeric, все лишнее удаляем)",
+     *           required=true,
+     *          @OA\Schema(
+     *              enum={"1 - Plus", "2 - Gold", "3 - Premium"},
+     *              example="2 - Gold",
+     *              type="string"
+     *          )
+     *     ),
+     *
+     *     @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\JsonContent(
+     *              ref="#/components/schemas/SuccessResponse",
+     *              example={
+     *                  "meta": {
+     *                      "error": null,
+     *                      "status": 200
+     *                  },
+     *                  "data": {
+     *                      "type": "Tinderone Plus+",
+     *                      "subscription_services": {
+     *                          {
+     *                              "description": "Безлимит лайков! Ставь столько лайков, сколько захочешь!",
+     *                              "image": "2,277103501936"
+     *                          }
+     *                      },
+     *                      "subscription_packages": {
+     *                          {
+     *                              "id": 1,
+     *                              "is_bestseller": false,
+     *                              "stock": 0,
+     *                              "term": "one_month",
+     *                              "price_per_month": "399"
+     *                          }
+     *                      }
+     *                  }
+     *              }
+     *          )
+     *     ),
+     *
+     *     @OA\Response(
+     *         @OA\JsonContent(ref="#/components/schemas/Unauthorized"),
+     *         description="Unauthorized",
+     *         response=401
+     *     )
+     * )
+     * @return JsonResponse
+     * @throws Exception
+     */
+    public function getSubscriptions($id, Request $request): JsonResponse
+    {
+        try {
+            $id = (int)preg_replace('/\D/', '', $id);
+            $gender = $request->customer['gender'];
+
+            $subscription = Subscriptions::with([
+                'services:id,subscription_id,description,image',
+                'packages.price'
+            ])
+                ->findOrFail($id);
+
+            $formattedSubscription = [
+                'type' => $subscription->type,
+                'subscription_services' => $subscription->services->map(function ($service) use ($gender) {
+                    return [
+                        'description' => $service->description,
+                        'image' => $service->image
+                    ];
+                }),
+                'subscription_packages' => $subscription->packages->map(function ($package) use ($gender) {
+                    return [
+                        'id' => $package->id,
+                        'is_bestseller' => (bool)$package->is_bestseller,
+                        'stock' => $package->stock,
+                        'term' => $package->term,
+                        'price_per_month' => (string)intval($package->price->{$gender} ?? 0)
+                    ];
+                })
+            ];
+
+            return $this->successResponse($formattedSubscription);
+        } catch (Exception $e) {
+            return $this->errorResponse(
+                $e->getMessage(),
+                (int)$e->getCode()
+            );
+        }
+    }
 
     /**
      * @param Request $request
@@ -34,7 +137,7 @@ class PricesController extends Controller
      *     ),
      *
      *     @OA\Response(
-     *          response=201,
+     *          response=200,
      *          description="Successful operation",
      *          @OA\JsonContent(
      *              ref="#/components/schemas/SuccessResponse",
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,26 +2,27 @@
 
 namespace App\Http\Controllers;
 
-use OpenApi\Annotations as OA;
-
 /**
  * @OA\Info(
- *      version="1.0.1",
- *      title="TinderOne API",
- *      description="Payment service documentaion",
- *      @OA\Contact(
- *          email="enternetacum@yandex.ru"
- *      ),
- *  )
+ *       version="1.0.1",
+ *       title="TinderOne API",
+ *       description="Payment service documentaion",
+ *       @OA\Contact(
+ *           email="enternetacum@yandex.ru"
+ *       ),
+ *   )
  * @OA\Tag(
- *      name="Customer Authorization",
- *  )
+ *       name="Customer Authorization",
+ *   )
  * @OA\Tag(
- *       name="Recommendations",
- *  )
+ *        name="Recommendations",
+ *   )
  * @OA\Tag(
- *      name="Payments",
+ *        name="Payments",
  *  )
+ *  @OA\Tag(
+ *       name="App Settings"
+ *   )
  */
 abstract class Controller
 {
