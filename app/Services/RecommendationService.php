9820635072bd1593813f269dfb6b11e5ea7d9715
peter6754 <p1312402@gmail.com>
2025-01-19 11:58:27 +0000

    Push Lpop

--- app/Services/RecommendationService.php
+++ app/Services/RecommendationService.php
@@ -60,7 +60,9 @@ class RecommendationService
         $key = "recommended:{$userId}:{$keyPart1}:{$keyPart2}:{$keyPart3}{$keyPart4}";
 
         try {
-            $forPage = Redis::lRange($key, 0, $this->recommendationsCacheSize - 1);
+            $forPage = Redis::command('LPOP', [$key, $this->recommendationsCacheSize]);
+//            $forPage = Redis::lRange($key, 0, $this->recommendationsCacheSize - 1);
+//            Redis::lTrim($key, $this->recommendationsCacheSize, -1);
 
             if (empty($forPage)) {
                 $fromDb = $this->getRecommendationsForCache($userId, $query, $this->recommendationsCacheSize);
