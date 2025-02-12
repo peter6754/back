17585d9cadb32fb5d95353cc51b6e12bf07407b4
peter6754 <p1312402@gmail.com>
2025-02-12 07:48:41 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@ git reset --hard
 git clean -fd
 git pull origin main --force
 chown -R www-data:www-data ./
+php composer.phar install
 php artisan view:clear && php artisan cache:clear && php artisan route:clear && php artisan config:clear && php artisan route:cache && php artisan config:cache
-npm install 
+npm install
 npm run build
