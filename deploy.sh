4476ecc3113805354662710d1d384a732ca49525
peter6754 <p1312402@gmail.com>
2025-02-21 07:15:26 +0000

    Update gitlab runner

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Инициализация node
-export NVM_DIR="/usr/local/nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-
 # Очистка локальных изменений и принудительное обновление из репозитория
 git reset --hard           # Сброс всех локальных изменений
 git pull origin main --force  # Принудительное обновление из ветки main
