f3dfd2ce6fe2e8231e45b1afc456edc4867023d4
peter6754 <p1312402@gmail.com>
2025-02-24 09:07:03 +0000

    Update device tokens

--- app/Models/UserDeviceToken.php
+++ app/Models/UserDeviceToken.php
@@ -47,16 +47,6 @@ class UserDeviceToken extends Model
     }
 
     /**
-     * Find device tokens by user ID.
-     * @param string $userId
-     * @return \Illuminate\Database\Eloquent\Collection
-     */
-    public static function findByUserId(string $userId)
-    {
-        return static::where('user_id', $userId)->get();
-    }
-
-    /**
      * Add a new device token for a user.
      * @param string $userId
      * @param array $query
@@ -95,16 +85,6 @@ class UserDeviceToken extends Model
         return (bool)static::where('user_id', $userId)
             ->where('token', $query['token'])
             ->delete();
-    }
-
-    /**
-     * Remove all device tokens for a user.
-     * @param string $userId
-     * @return bool
-     */
-    public static function removeAllTokens(string $userId): bool
-    {
-        return (bool)static::where('user_id', $userId)->delete();
     }
 
     /**
