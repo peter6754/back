88a82d5d6db85651bd43880ab592312513d5ef5a
peter6754 <p1312402@gmail.com>
2025-02-24 06:11:05 +0000

    Update api docs

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,39 +3,29 @@
 namespace App\Http\Controllers;
 
 /**
- * @OA\Info(
- *       version="1.0.1",
- *       title="TinderOne API",
- *       description="Payment service documentaion",
- *       @OA\Contact(
- *           email="enternetacum@yandex.ru"
- *       ),
- *   )
- * @OA\Server(
- *     url="http://127.0.0.1:8000",
- *     description="Local development server"
- * )
- * @OA\SecurityScheme(
- *     securityScheme="bearerAuth",
- *     type="http",
- *     scheme="bearer",
- *     bearerFormat="JWT"
- * )
- * @OA\Tag(
- *       name="Customer Authorization",
- *   )
- * @OA\Tag(
- *        name="Recommendations",
- *   )
- * @OA\Tag(
- *        name="Payments",
+ *  @OA\Info(
+ *      version="1.0.1",
+ *      title="TinderOne API",
+ *      description="Payment service documentaion",
+ *      @OA\Contact(
+ *          email="enternetacum@yandex.ru"
+ *      )
  *  )
  *  @OA\Tag(
- *       name="App Settings"
- *   )
- * @OA\Tag(
- *        name="User Settings"
- *    )
+ *      name="Customer Authorization",
+ *  )
+ *  @OA\Tag(
+ *      name="Recommendations",
+ *  )
+ *  @OA\Tag(
+ *      name="Payments",
+ *  )
+ *  @OA\Tag(
+ *      name="App Settings"
+ *  )
+ *  @OA\Tag(
+ *      name="User Settings"
+ *  )
  */
 abstract class Controller
 {
