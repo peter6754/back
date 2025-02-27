025edd7c86761ee07b2206562de2f7ebbbce6f22
peter6754 <p1312402@gmail.com>
2025-02-27 21:04:51 +0000

    Update cors

--- config/cors.php
+++ config/cors.php
@@ -14,15 +14,15 @@ return [
     | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
     |
     */
-    'allowed_origins_patterns' => [
-        '/\.tinderone\.app$/',
-        '/\.tinderone\.ru$/'
-    ],
     'allowed_origins' => [
         'http://localhost:5173',
         'http://localhost:3000',
         'http://localhost:8000',
         'http://127.0.0.1:8000'
+    ],
+    'allowed_origins_patterns' => [
+        '/\.tinderone\.app$/',
+        '/\.tinderone\.ru$/'
     ],
     'allowed_headers' => [
         'X-Requested-With',
