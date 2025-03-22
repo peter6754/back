514896460b4252b07f95fdbdf272c473ed2c0a9e
peter6754 <p1312402@gmail.com>
2025-03-22 06:14:11 +0000

    Changed sms balance format.

--- resources/views/vendor/backpack/theme-tabler/dashboard.blade.php
+++ resources/views/vendor/backpack/theme-tabler/dashboard.blade.php
@@ -49,6 +49,7 @@
     $smsBalance = "0.00";
     if (app()->environment('production')) {
         $smsBalance = (new \App\Services\External\GreenSMSService)->getBalance();
+        $smsBalance = number_format($balance, 2, '.', '');
     }
     $smsBalance = "<br>$smsBalance<br>";
     $usersByStore = SecondaryuserCrudController::getNewUsersByStore();
