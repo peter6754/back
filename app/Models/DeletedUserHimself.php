3c51676b262ae23b1944814c9374a2b62a6c9794
daitril <daitril@gmail.com>
2025-03-27 20:02:37 +0200

    Added deleted user count and button delete count. Added informers

--- app/Models/DeletedUserHimself.php
+++ app/Models/DeletedUserHimself.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
+use DB;
 use Illuminate\Database\Eloquent\Model;
 
 class DeletedUserHimself extends Model
@@ -15,5 +17,27 @@ class DeletedUserHimself extends Model
     public static function countDeletedUsers()
     {
         return self::count();
+    }
+
+    public static function getLastCheckStats()
+    {
+        $yesterday = Carbon::now('Europe/Moscow')->subDay()->startOfDay()->setTimezone('UTC');
+        $todayStart = Carbon::now('Europe/Moscow')->startOfDay()->setTimezone('UTC');
+        $todayEnd = Carbon::today()->endOfDay();
+
+        $todayCount = self::whereBetween(
+            DB::raw("JSON_UNQUOTE(JSON_EXTRACT(user_data, '$.last_check'))"),
+            [$todayStart, $todayEnd]
+        )->count();
+
+        $yesterdayCount = self::whereBetween(
+            DB::raw("JSON_UNQUOTE(JSON_EXTRACT(user_data, '$.last_check'))"),
+            [$yesterday, $todayStart]
+        )->count();
+
+        return [
+            'today' => $todayCount,
+            'yesterday' => $yesterdayCount,
+        ];
     }
 }
