31c58f6eab22df454b586f477d0409cd044535e4
peter6754 <p1312402@gmail.com>
2025-04-24 20:35:23 +0000

    Add new languages

--- config/locales.php
+++ config/locales.php
@@ -3,7 +3,20 @@
 return [
     'default_locale' => 'ru',
     'available_locales' => [
-        'ru' => 'Русский',
+        'af' => 'Afrikaans',
         'en' => 'English',
+        'es' => 'Español',
+        'fr' => 'Français',
+        'hy' => 'Հայերեն',
+        'it' => 'Italiano',
+        'kk' => 'Қазақ',
+        'pt' => 'Português',
+        'ro' => 'Română',
+        'ru' => 'Русский',
+        'sw' => 'Kiswahili',
+        'tg' => 'Тоҷикӣ',
+        'tr' => 'Türkçe',
+        'uk' => 'Українська',
+        'uz' => 'Oʻzbek',
     ]
 ];
