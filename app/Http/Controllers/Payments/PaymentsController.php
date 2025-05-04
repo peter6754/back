65230ae56284bc390e4467eb109c0eed70dbcbca
Oleg Filippov <oleg@polbox.tv>
2025-05-04 13:24:11 +0300

    Add default unauthorized method

--- app/Http/Controllers/Payments/PaymentsController.php
+++ app/Http/Controllers/Payments/PaymentsController.php
@@ -2,16 +2,14 @@
 
 namespace App\Http\Controllers\Payments;
 
-use Exception;
-use App\Models\User;
-use App\Models\Secondaryuser;
-use Illuminate\Http\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use App\Http\Traits\ApiResponseTrait;
 use App\Http\Controllers\Controller;
 use App\Services\RobokassaService;
+use Illuminate\Http\JsonResponse;
+use App\Models\Secondaryuser;
 use App\Models\Transactions;
-use App\Services\JwtService;
+use Exception;
 
 class PaymentsController extends Controller
 {
@@ -28,7 +26,7 @@ class PaymentsController extends Controller
     protected Transactions $transactions;
 
     /**
-     *
+     * @throws Exception
      */
     public function __construct()
     {
@@ -41,23 +39,33 @@ class PaymentsController extends Controller
 
     public function servicePackage()
     {
+        // Checking auth user
+        $this->checkingAuth();
 
     }
 
     public function subscription()
     {
+        // Checking auth user
+        $this->checkingAuth();
 
     }
 
     public function recurring()
     {
+        // Checking auth user
+        $this->checkingAuth();
 
     }
 
     public function gift()
     {
+        // Checking auth user
+        $this->checkingAuth();
 
     }
+
+
 
     /**
      * @param $id
@@ -66,14 +74,10 @@ class PaymentsController extends Controller
      */
     public function status($id): JsonResponse
     {
-        if (!Secondaryuser::getUser()) {
-            return $this->errorResponse(
-                Response::$statusTexts[Response::HTTP_UNAUTHORIZED],
-                4010,
-                Response::HTTP_UNAUTHORIZED
-            );
-        }
+        // Checking auth user
+        $this->checkingAuth();
 
+        // Get transaction
         if (!$transaction = Transactions::select('status')->find($id)) {
             return $this->errorResponse(
                 "Transaction not found",
@@ -82,8 +86,27 @@ class PaymentsController extends Controller
             );
         }
 
+        // Return status
         return $this->successResponse([
             'status' => $transaction->status
         ]);
+    }
+
+    /**
+     * @return array|JsonResponse
+     * @throws Exception
+     */
+    private function checkingAuth(): JsonResponse|array
+    {
+        $getUser = Secondaryuser::getUser();
+        if (empty($getUser)) {
+            $this->errorResponse(
+                Response::$statusTexts[Response::HTTP_UNAUTHORIZED],
+                4010,
+                Response::HTTP_UNAUTHORIZED
+            )->send();
+            die();
+        }
+        return $getUser;
     }
 }
