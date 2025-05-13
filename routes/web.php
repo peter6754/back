1d34c7b63f7261134274067b5c6dfc75d8f2aa8b
peter6754 <p1312402@gmail.com>
2025-05-13 07:46:28 +0000

    Update config URL

--- routes/web.php
+++ routes/web.php
@@ -147,12 +147,8 @@ Route::prefix('auth')->group(function () {
 
             $platformString = ($provider !== 'google' && $platform ? "/".$platform : "");
 
-            config([
-                "services.{$provider}.redirect" => config("services.{$provider}.redirect").$platformString
-            ]);
-
             $socialProvider = Socialite::driver($provider)->redirectUrl(
-                url(config("services.{$provider}.redirect"))
+                url(config("services.{$provider}.redirect").$platformString)
             );
 
             if ($provider === "telegram") {
