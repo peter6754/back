631f53ab75e7a2c7a77fcdc164b78a9443620f61
Oleg Filippov <oleg@polbox.tv>
2025-05-26 22:38:28 +0300

    Update telescope

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -2,31 +2,42 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Gate;
-use Laravel\Telescope\IncomingEntry;
-use Laravel\Telescope\Telescope;
 use Laravel\Telescope\TelescopeApplicationServiceProvider;
+use Laravel\Telescope\IncomingEntry;
+use Illuminate\Support\Collection;
+use Laravel\Telescope\Telescope;
 
 class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
 {
+    /**
+     * Checking backpack admin authorisation
+     */
+    protected function authorization(): void
+    {
+        Telescope::auth(function ($request) {
+            return backpack_auth()->check() && backpack_user()->hasRole('Superadmin');
+        });
+    }
+
     /**
      * Register any application services.
      */
     public function register(): void
     {
-        // Telescope::night();
-
         $this->hideSensitiveRequestDetails();
 
-        $isLocal = $this->app->environment('production');
+        Telescope::filterBatch(function (Collection $entries) {
+            if ($this->app->environment('local')) {
+                return true;
+            }
 
-        Telescope::filter(function (IncomingEntry $entry) use ($isLocal) {
-            return $isLocal ||
-                $entry->isReportableException() ||
-                $entry->isFailedRequest() ||
-                $entry->isFailedJob() ||
-                $entry->isScheduledTask() ||
-                $entry->hasMonitoredTag();
+            return $entries->contains(function (IncomingEntry $entry) {
+                return $entry->isReportableException() ||
+                    $entry->isFailedJob() ||
+                    $entry->isScheduledTask() ||
+                    $entry->isSlowQuery() ||
+                    $entry->hasMonitoredTag();
+            });
         });
     }
 
@@ -35,24 +46,12 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
      */
     protected function hideSensitiveRequestDetails(): void
     {
-        if ($this->app->environment('production')) {
-            return;
-        }
-
         Telescope::hideRequestParameters(['_token']);
-
         Telescope::hideRequestHeaders([
-            'cookie',
             'x-csrf-token',
             'x-xsrf-token',
+            'cookie',
         ]);
     }
 
-    protected function authorization()
-    {
-        Telescope::auth(function ($request) {
-            // Доступ только для авторизованных пользователей Backpack
-            return backpack_auth()->check() && backpack_user()->hasRole('Superadmin');
-        });
-    }
 }
