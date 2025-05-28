49dbf5bf6ecd3c340a49863348f088bcb0174190
Oleg Filippov <oleg@polbox.tv>
2025-05-28 19:48:14 +0300

    Update configs

--- config/services.php
+++ config/services.php
@@ -37,13 +37,13 @@ return [
 
     // Oauth settings
     'google' => [
-        'client_id' => env('GOOGLE_CLIENT_ID', 'test'),
+        'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
         'redirect' => env('GOOGLE_REDIRECT_URI'),
     ],
 
     'apple' => [
-        'client_id' => env('APPLE_CLIENT_ID', 'test'),
+        'client_id' => env('APPLE_CLIENT_ID'),
         'client_secret' => env('APPLE_CLIENT_SECRET'),
         'redirect' => env('APPLE_REDIRECT_URI'),
         'team_id' => env('APPLE_TEAM_ID'),
