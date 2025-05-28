18b696bab9ce8faa2553d24081fa2e5af8fdea02
Oleg Filippov <oleg@polbox.tv>
2025-05-28 14:22:16 +0300

    Add token

--- config/greensms.php
+++ config/greensms.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
-    'sms_user' => env('GREENSMS_USER', "USERNAME"),
-    'sms_pass' => env('GREENSMS_PASS', "PASSWORD"),
+    'token' => env('GREENSMS_TOKEN', 'MY_APPLICATION_TOKEN'),
+    'sms_user' => env('GREENSMS_USER', 'USERNAME'),
+    'sms_pass' => env('GREENSMS_PASS', 'PASSWORD'),
 ];
