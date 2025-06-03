e140cf3bb1efa1acaac8c597ed09930a80701047
Oleg Filippov <oleg@polbox.tv>
2025-06-03 16:24:37 +0300

    Add telescope custom rules

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -2,10 +2,12 @@
 
 namespace App\Providers;
 
+use Laravel\Telescope\EntryType;
 use Laravel\Telescope\TelescopeApplicationServiceProvider;
 use Laravel\Telescope\IncomingEntry;
 use Illuminate\Support\Collection;
 use Laravel\Telescope\Telescope;
+use function Symfony\Component\String\b;
 
 class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
 {
@@ -27,21 +29,31 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
         $this->hideSensitiveRequestDetails();
 
         Telescope::filterBatch(function (Collection $entries) {
-            if (
-                $this->app->environment('production') ||
-                $this->app->environment('local')) {
+            if ($this->app->environment('local')) {
                 return true;
             }
 
             // Temperary not working
             return $entries->contains(function (IncomingEntry $entry) {
-                return $entry->isReportableException() ||
+                return $this->myCustomRules($entry) ||
+                    $entry->isReportableException() ||
                     $entry->isScheduledTask() ||
                     $entry->isFailedJob() ||
-                    $entry->isSlowQuery() ||
-                    $entry->isRequest();
+                    $entry->isSlowQuery();
             });
         });
+    }
+
+    /**
+     * @param IncomingEntry $entry
+     * @return bool
+     */
+    private function myCustomRules(IncomingEntry $entry): bool
+    {
+        return match ($entry->type) {
+            EntryType::REQUEST => ($entry->content['response_status'] ?? 200) >= 400,
+            default => false
+        };
     }
 
     /**
