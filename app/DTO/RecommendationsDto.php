e07b910b8de19d79025003dc506ed53042327818
Oleg Filippov <oleg@polbox.tv>
2025-07-22 09:25:52 +0300

    Add new payment method configs

--- app/DTO/RecommendationsDto.php
+++ app/DTO/RecommendationsDto.php
@@ -3,7 +3,6 @@
 namespace App\DTO;
 
 use Spatie\LaravelData\Data;
-use Illuminate\Support\Facades\Request;
 
 class RecommendationsDto extends Data
 {
--- config/payments.php
+++ config/payments.php
@@ -11,5 +11,12 @@ return [
         'password1_test' => env('ROBOKASSA_PASSWORD1_TEST'),
         'password2_test' => env('ROBOKASSA_PASSWORD2_TEST'),
         'isTest' => (int)env('ROBOKASSA_TEST', 0),
+    ],
+
+    'unitpay' => [
+        'project_id' => env('UNITPAY_PROJECT_ID'),
+        'public_key' => env('UNITPAY_PUBLIC_KEY'),
+        'secret_key' => env('UNITPAY_SECRET_KEY'),
+        'isTest' => env('UNITPAY_TEST_MODE', false),
     ]
 ];
